4d03e12ebc188f5bb5fe7219ea2e5adf25491b7b
PeDitX <pedram.ale@me.com>
2025-02-12 20:24:31 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,15 +39,15 @@ fi
 curl -L -o /usr/bin/aircast "$BINARY_URL" || wget -O /usr/bin/aircast "$BINARY_URL"
 chmod +x /usr/bin/aircast
 
+# Ensure the process is stopped before replacing the binary
+killall aircast
+
 # Create AirCast config file
 cat << EOF > /etc/aircast.conf
 interface=br-lan
 device_name=AirCastDevice
 ip=$(ip addr show br-lan | grep inet | awk '{print $2}' | cut -d/ -f1)
 EOF
-
-# Make sure aircast reads the correct configuration file
-sed -i 's|/usr/bin/aircast|/usr/bin/aircast --config /etc/aircast.conf|' /etc/init.d/aircast
 
 # Create service startup script
 cat << 'EOF' > /etc/init.d/aircast
